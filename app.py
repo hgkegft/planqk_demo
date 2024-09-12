81ca64056aa25fe14400fd6a884f6ab068178599
Henning Krause <henning.krause@gft.com>
2024-09-12 14:12:39 +0200

    Update UI.

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
+import json
 import os
 import sys
 
 import gradio as gr
 from loguru import logger
 from planqk.service.client import PlanqkServiceClient
-
 
 logging_level = os.environ.get("LOG_LEVEL", "DEBUG")
 logger.configure(handlers=[{"sink": sys.stdout, "level": logging_level}])
@@ -17,18 +17,71 @@ service_endpoint = os.getenv(
     "https://gateway.platform.planqk.de/anaqor/quantum-random-number-generator/1.0.0",
 )
 
-
 title = "A PlanQK Demo using Gradio!"
 description = '<div align="center"> <h1>A descriptive description!</h1> </div>'
 
 
-def run() -> str:
-    # client = PlanqkServiceClient(service_endpoint, consumer_key, consumer_secret)
-    # data = {"n_numbers": n_numbers}
-    # params = {"n_bits": 4, "backend": "qasm_simulator"}
-    # job = client.start_execution(data=data, params=params)
-    # result = client.get_result(job.id)
-    return "txt"
+def upload_json_file(file):
+    file_path = file.name
+    parts = file_path.split("/")
+    filename = parts[-1]
+    parts = filename.split(".")
+    if len(parts) < 2:
+        raise Exception("File name is not long enough.")
+
+    file_extension = parts[-1]
+    if file_extension not in ["json"]:
+        raise Exception("File name is not a JSON file.")
+
+    with open(file_path) as f:
+        data = json.load(f)
+
+    keys = ["X_train", "y_train"]
+    for key in keys:
+        if key not in data.keys():
+            raise Exception("Mandatory key: {key} not in JSON file.")
+
+    return file_path, data
+
+
+def train(
+    data_file,
+    regression_choice,
+    rescaling_choice,
+    encoding_choice,
+    dim_reduction,
+    problem_type,
+    mode,
+    time_budget,
+):
+    file_path = data_file.name
+    with open(file_path) as f:
+        data = json.load(f)
+
+    keys = ["X_train", "y_train"]
+    for key in keys:
+        if key not in data.keys():
+            raise Exception("Mandatory key: {key} not in JSON file.")
+
+    custom_config = {
+        "autoqml_lib.search_space.regression.RegressionChoice__choice": regression_choice,
+        "autoqml_lib.search_space.preprocessing.rescaling.RescalingChoice__choice": rescaling_choice,
+        "autoqml_lib.search_space.preprocessing.encoding.EncoderChoice__choice": encoding_choice,
+        "autoqml_lib.search_space.preprocessing.dim_reduction.DimReductionChoice__choice": dim_reduction,
+        "autoqml_lib.search_space.preprocessing.dim_reduction.autoencoder.Autoencoder__latent_dim": 10,
+    }
+
+    params = dict()
+    params["custom_config"] = custom_config
+    params["mode"] = mode
+    params["time_budget_for_this_task"] = time_budget
+    params["problem_type"] = problem_type
+
+    client = PlanqkServiceClient(service_endpoint, consumer_key, consumer_secret)
+    job = client.start_execution(data=data, params=params)
+    result = client.get_result(job.id)
+
+    return result, data, params
 
 
 with gr.Blocks(title=title) as demo:
@@ -55,10 +108,56 @@ with gr.Blocks(title=title) as demo:
                     label="Dimension reduction", choices=["pca", "autoencoder"]
                 )
             with gr.Column():
-                time_budget_for_this_task = gr.Number(
-                    label="Time budget(seconds)", value=60
+                time_budget = gr.Number(label="Time budget(seconds)", value=60)
+                problem_type = gr.Dropdown(label="Problem type", choices=["classification", "regression"])
+
+                data_file = gr.File()
+                upload_button = gr.UploadButton(
+                    "Click to Upload a File",
+                    file_types=["text"],
+                    file_count="single",
                 )
-                problem_type = gr.Dropdown(choices=["classification", "regression"])
+                with gr.Accordion("Inspect Data", open=False):
+                    data_json_box = gr.JSON()
+
+                upload_button.upload(
+                    upload_json_file, upload_button, [data_file, data_json_box]
+                )
+
+        with gr.Row():
+            with gr.Column():
+                train_button = gr.Button(value="Train")
+            with gr.Column():
+                ...
+            with gr.Column():
+                ...
+            with gr.Column():
+                ...
+
+        with gr.Accordion("Inspect Data", open=False):
+            with gr.Row():
+                send_data_json_box = gr.JSON()
+                send_params_json_box = gr.JSON()
+
+        with gr.Column():
+            result_json_box = gr.JSON()
+
+            mode = gr.Text("train", visible=False)
+            train_button.click(
+                train,
+                inputs=[
+                    data_file,
+                    regression_choice,
+                    rescaling_choice,
+                    encoding_choice,
+                    dim_reduction,
+                    problem_type,
+                    mode,
+                    time_budget,
+                ],
+                outputs=[result_json_box, send_data_json_box, send_params_json_box],
+                api_name="train",
+            )
     with gr.Tab("Prediction"):
         ...
 
