842541b267f4065e9adbe737e3222a5c516cc38d
Henning Krause <henning.krause@gft.com>
2024-10-01 14:08:17 +0200

    Test data pools as input.

--- lib.py
+++ lib.py
@@ -57,24 +57,24 @@ def execute_on_planqk(data, params):
 
 
 def train(
-    data_file,
-    regression_choice,
-    rescaling_choice,
-    encoding_choice,
-    dim_reduction,
-    n_reduction_dims,
-    problem_type,
-    mode,
-    time_budget,
+        data_file,
+        regression_choice,
+        rescaling_choice,
+        encoding_choice,
+        dim_reduction,
+        n_reduction_dims,
+        problem_type,
+        mode,
+        time_budget,
 ):
     file_path = data_file.name
     with open(file_path) as f:
         data = json.load(f)
 
-    keys = ["X_train", "y_train"]
-    for key in keys:
-        if key not in data.keys():
-            raise Exception("Mandatory key: {key} not in JSON file.")
+    # keys = ["X_train", "y_train"]
+    # for key in keys:
+    #     if key not in data.keys():
+    #         raise Exception("Mandatory key: {key} not in JSON file.")
 
     custom_config = {
         "autoqml_lib.search_space.regression.RegressionChoice__choice": regression_choice,
@@ -106,10 +106,10 @@ def predict(data_file, result_json_box_train_output, mode):
     with open(file_path) as f:
         data = json.load(f)
 
-    keys = ["X_test", "y_test"]
-    for key in keys:
-        if key not in data.keys():
-            raise Exception("Mandatory key: {key} not in JSON file.")
+    # keys = ["X_test", "y_test"]
+    # for key in keys:
+    #     if key not in data.keys():
+    #         raise Exception("Mandatory key: {key} not in JSON file.")
 
     params = dict()
     params["mode"] = mode
@@ -121,15 +121,15 @@ def predict(data_file, result_json_box_train_output, mode):
 
 
 def create_train_data_and_params(
-    data_file,
-    regression_choice,
-    rescaling_choice,
-    encoding_choice,
-    dim_reduction,
-    n_reduction_dims,
-    problem_type,
-    mode,
-    time_budget,
+        data_file,
+        regression_choice,
+        rescaling_choice,
+        encoding_choice,
+        dim_reduction,
+        n_reduction_dims,
+        problem_type,
+        mode,
+        time_budget,
 ):
     file_path = data_file.name
     with open(file_path) as f:
