fb5622557215da5e00857139016ff40fd2024a04
Henning Krause <henning.krause@gft.com>
2024-10-14 14:01:16 +0200

    Split up code.

--- train.py
+++ train.py
@@ -1,26 +1,35 @@
 import json
 
-from lib import ref_datasets, execute_on_planqk, execute_with_reference_data
+from lib import ref_datasets, execute_on_planqk, execute_with_reference_data, execute_with_upload_data
 
 
 def train_trigger(
         regression_choice,
+        classification_choice,
         rescaling_choice,
+        rescaling_min_max_feature_range,
+        rescaling_normalization_norm,
         encoding_choice,
+        one_hot_min_frequency,
+        one_hot_max_categories,
         dim_reduction,
         n_reduction_dims,
+        time_budget,
         problem_type,
         mode,
-        time_budget,
         data_file=None,
         data_ref_identifier=None,
+        data_pool_reference=None,
 ):
     custom_config = {
         "autoqml_lib.search_space.regression.RegressionChoice__choice": regression_choice,
+        "autoqml_lib.search_space.classification.ClassificationChoice__choice": classification_choice,
         "autoqml_lib.search_space.preprocessing.rescaling.RescalingChoice__choice": rescaling_choice,
+        "autoqml_lib.search_space.preprocessing.rescaling.min_max_scaling.MinMaxScaling.__feature_range": rescaling_min_max_feature_range,
+        "autoqml_lib.search_space.preprocessing.rescaling.normalization.Normalization.__norm": rescaling_normalization_norm,
         "autoqml_lib.search_space.preprocessing.encoding.EncoderChoice__choice": encoding_choice,
-        "autoqml_lib.search_space.preprocessing.encoding.one_hot.OneHotEncoder__max_categories": 17,
-        "autoqml_lib.search_space.preprocessing.encoding.one_hot.OneHotEncoder__min_frequency": 1,
+        "autoqml_lib.search_space.preprocessing.encoding.one_hot.OneHotEncoder__max_categories": one_hot_max_categories,
+        "autoqml_lib.search_space.preprocessing.encoding.one_hot.OneHotEncoder__min_frequency": one_hot_min_frequency,
         "autoqml_lib.search_space.data_cleaning.imputation.ImputationChoice__choice": "no-op",
         "autoqml_lib.search_space.preprocessing.dim_reduction.DimReductionChoice__choice": dim_reduction,
         "autoqml_lib.search_space.preprocessing.dim_reduction.autoencoder.Autoencoder__latent_dim": int(
@@ -35,51 +44,45 @@ def train_trigger(
     params["time_budget_for_this_task"] = int(time_budget)
     params["problem_type"] = problem_type
 
-    if data_file is None:
+    if data_ref_identifier is not None:
         data_ref = ref_datasets[data_ref_identifier]
         return execute_with_reference_data(data_ref, params)
-    elif data_ref_identifier is None:
-        return execute_with_reference_data(data_file, params)
-
-
-def train_with_upload_data(
-        data_file,
-        params
-):
-    file_path = data_file.name
-    with open(file_path) as f:
-        data = json.load(f)
-
-    result = execute_on_planqk(data, params, data_ref=None)
-
-    return result
-
-
-def train_with_reference_data(
-        data_ref,
-        params
-):
-    return execute_on_planqk(data=None, params=params, data_ref=data_ref)
+    elif data_pool_reference is not None:
+        return execute_with_reference_data(data_pool_reference, params)
+    elif data_file is not None:
+        file_path = data_file.name
+        with open(file_path) as f:
+            data = json.load(f)
+        return execute_with_upload_data(data, params)
 
 
 def create_train_data_and_params(
         regression_choice,
+        classification_choice,
         rescaling_choice,
+        rescaling_min_max_feature_range,
+        rescaling_normalization_norm,
         encoding_choice,
+        one_hot_min_frequency,
+        one_hot_max_categories,
         dim_reduction,
         n_reduction_dims,
+        time_budget,
         problem_type,
         mode,
-        time_budget,
         data_file=None,
         data_ref_identifier=None,
+        data_pool_reference=None,
 ):
     custom_config = {
         "autoqml_lib.search_space.regression.RegressionChoice__choice": regression_choice,
+        "autoqml_lib.search_space.classification.ClassificationChoice__choice": classification_choice,
         "autoqml_lib.search_space.preprocessing.rescaling.RescalingChoice__choice": rescaling_choice,
+        "autoqml_lib.search_space.preprocessing.rescaling.min_max_scaling.MinMaxScaling.__feature_range": rescaling_min_max_feature_range,
+        "autoqml_lib.search_space.preprocessing.rescaling.normalization.Normalization.__norm": rescaling_normalization_norm,
         "autoqml_lib.search_space.preprocessing.encoding.EncoderChoice__choice": encoding_choice,
-        "autoqml_lib.search_space.preprocessing.encoding.one_hot.OneHotEncoder__max_categories": 17,
-        "autoqml_lib.search_space.preprocessing.encoding.one_hot.OneHotEncoder__min_frequency": 1,
+        "autoqml_lib.search_space.preprocessing.encoding.one_hot.OneHotEncoder__max_categories": one_hot_max_categories,
+        "autoqml_lib.search_space.preprocessing.encoding.one_hot.OneHotEncoder__min_frequency": one_hot_min_frequency,
         "autoqml_lib.search_space.data_cleaning.imputation.ImputationChoice__choice": "no-op",
         "autoqml_lib.search_space.preprocessing.dim_reduction.DimReductionChoice__choice": dim_reduction,
         "autoqml_lib.search_space.preprocessing.dim_reduction.autoencoder.Autoencoder__latent_dim": int(
@@ -94,12 +97,14 @@ def create_train_data_and_params(
     params["time_budget_for_this_task"] = int(time_budget)
     params["problem_type"] = problem_type
 
-    if data_file is not None:
+    if data_ref_identifier is not None:
+        data = ref_datasets[data_ref_identifier]
+    elif data_pool_reference is not None:
+        data = data_pool_reference
+    elif data_file is not None:
         file_path = data_file.name
         with open(file_path) as f:
             data = json.load(f)
-    elif data_ref_identifier is not None:
-        data = ref_datasets[data_ref_identifier]
     else:
         data = dict()
 
---
+++ ui.py
@@ -0,0 +1,251 @@
+import gradio as gr
+
+from lib import (
+    upload_json_file,
+    ref_identifier
+)
+from predict import create_predict_data_and_params, predict_trigger
+from train import train_trigger, create_train_data_and_params
+
+
+def get_config_elements():
+    with gr.Accordion("Regression", open=False):
+        regression_choice = gr.Dropdown(
+            label="Regression method",
+            choices=["svr", "qsvr"],
+            value="qsvr",
+        )
+    with gr.Accordion("Classification", open=False):
+        classification_choice = gr.Dropdown(
+            label="Classification method",
+            choices=["qgpc", "qnn", "qsvc", "random_forest", "svc"],
+            value="qsvc",
+        )
+    with gr.Accordion("Rescaling", open=False):
+        rescaling_choice = gr.Dropdown(
+            label="Rescaling method",
+            choices=[
+                "standard_scaling",
+                "normalization",
+                "min_max_scaling",
+                "no-op",
+            ],
+            value="standard_scaling",
+        )
+        rescaling_min_max_feature_range = gr.Number(label="MinMax Feature Range", minimum=0.0, value=0.5, maximum=1.0)
+        rescaling_normalization_norm = gr.Dropdown(
+            label="Normalization norm",
+            choices=[
+                "l1",
+                "l2",
+                "max",
+            ],
+            value="l1",
+        )
+    with gr.Accordion("Encoding", open=False):
+        encoding_choice = gr.Dropdown(
+            label="Encoding method",
+            choices=["categorical", "one-hot", "no-op"],
+            value="one-hot",
+        )
+        one_hot_min_frequency = gr.Number(label="One-Hot Min Frequency", minimum=1, value=1)
+        one_hot_max_categories = gr.Number(label="One-Hot Max categories", minimum=1, value=1)
+    with gr.Accordion("Rescaling", open=False):
+        ...
+    with gr.Accordion("Down sampling", open=False):
+        ...
+    with gr.Accordion("Dimension reduction", open=False):
+        dim_reduction = gr.Dropdown(
+            label="Dimension reduction method",
+            choices=["pca", "autoencoder"],
+            value="autoencoder",
+        )
+        n_reduction_dims = gr.Number(label="Reduction dims", value=5)
+    with gr.Accordion("General", open=False):
+        time_budget = gr.Number(label="Time budget(seconds)", value=5)
+        problem_type = gr.Dropdown(
+            label="Problem type",
+            choices=["classification", "regression"],
+            value="regression",
+        )
+
+    return (regression_choice,
+            classification_choice,
+            rescaling_choice,
+            rescaling_min_max_feature_range,
+            rescaling_normalization_norm,
+            encoding_choice,
+            one_hot_min_frequency,
+            one_hot_max_categories,
+            dim_reduction,
+            n_reduction_dims,
+            time_budget,
+            problem_type)
+
+
+def handle_dataset_reference():
+    with gr.Tab("Reference data"):
+        dataset_reference = gr.Dropdown(
+            label="Reference Datasets",
+            choices=ref_identifier,
+            value=None
+        )
+        data_pool_reference = gr.Textbox(label="Reference data pool")
+
+    return dataset_reference, data_pool_reference
+
+
+def handle_data_upload():
+    with gr.Tab("Upload data"):
+        train_data_file = gr.File()
+        upload_button = gr.UploadButton(
+            "Click to upload a file",
+            file_types=["text"],
+            file_count="single",
+        )
+        with gr.Accordion("Inspect Data", open=False):
+            data_json_box = gr.JSON()
+
+        upload_button.upload(
+            upload_json_file, upload_button, [train_data_file, data_json_box]
+        )
+    return train_data_file, upload_button
+
+
+def get_inspect_block():
+    with gr.Accordion("Inspect data", open=False):
+        with gr.Row():
+            gr.Markdown("Data", scale=3)
+            gr.Markdown("Params", scale=4)
+        with gr.Row():
+            send_data_json_box = gr.JSON(scale=3)
+            send_params_json_box = gr.JSON(scale=4)
+    return send_data_json_box, send_params_json_box
+
+
+def training_ui():
+    with gr.Tab("Training"):
+        with gr.Row():
+            with gr.Column():
+                config_elements = get_config_elements()
+            with gr.Column():
+                dataset_reference, data_pool_reference = handle_dataset_reference()
+                train_data_file, upload_button = handle_data_upload()
+
+        with gr.Row():
+            with gr.Column():
+                train_button = gr.Button(value="Train")
+            with gr.Column():
+                ...
+            with gr.Column():
+                ...
+            with gr.Column():
+                ...
+
+        send_data_json_box, send_params_json_box = get_inspect_block()
+
+        with gr.Column():
+            with gr.Accordion("Result", open=False):
+                result_json_box_train = gr.JSON()
+
+        mode = gr.Text("train", visible=False)
+        train_button.click(
+            train_trigger,
+            inputs=[
+                *config_elements,
+                mode,
+                train_data_file,
+                dataset_reference,
+                data_pool_reference,
+            ],
+            outputs=[result_json_box_train],
+            api_name="train",
+        )
+        gr.on(
+            [train_button.click],
+            create_train_data_and_params,
+            inputs=[
+                *config_elements,
+                mode,
+                train_data_file,
+                dataset_reference,
+                data_pool_reference,
+            ],
+            outputs=[
+                send_params_json_box,
+                send_data_json_box,
+            ],
+        )
+    return result_json_box_train
+
+
+def prediction_ui(result_json_box_train):
+    with gr.Tab("Prediction"):
+        with gr.Accordion("Result data", open=False):
+            result_json_box_train_output = gr.JSON()
+            gr.on(
+                [result_json_box_train.change],
+                lambda value: value,
+                inputs=[result_json_box_train],
+                outputs=[result_json_box_train_output],
+            )
+
+        gr.Markdown(value="Specify data")
+
+        with gr.Row():
+            with gr.Column():
+                predict_data_file = gr.File()
+                upload_button = gr.UploadButton(
+                    "Click to upload a file",
+                    file_types=["text"],
+                    file_count="single",
+                )
+                with gr.Accordion("Inspect Data", open=False):
+                    data_json_box = gr.JSON()
+
+                upload_button.upload(
+                    upload_json_file, upload_button, [predict_data_file, data_json_box]
+                )
+            with gr.Column():
+                ...
+
+        with gr.Row():
+            with gr.Column():
+                predict_button = gr.Button(value="Predict")
+            with gr.Column():
+                ...
+            with gr.Column():
+                ...
+            with gr.Column():
+                ...
+
+        send_data_json_box, send_params_json_box = get_inspect_block()
+
+        with gr.Column():
+            with gr.Accordion("Result", open=False):
+                result_json_box_predict = gr.JSON()
+
+            mode = gr.Text("predict", visible=False)
+            predict_button.click(
+                predict_trigger,
+                inputs=[
+                    result_json_box_train_output,
+                    mode,
+                    predict_data_file,
+                ],
+                outputs=[result_json_box_predict],
+                api_name="predict",
+            )
+            gr.on(
+                [predict_button.click],
+                create_predict_data_and_params,
+                inputs=[
+                    result_json_box_train_output,
+                    mode,
+                    predict_data_file,
+                ],
+                outputs=[
+                    send_params_json_box,
+                    send_data_json_box,
+                ],
+            )
