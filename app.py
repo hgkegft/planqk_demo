50f44f12f3e28d600932d09aaa5bfd7a18a3800f
Henning Krause <henning.krause@gft.com>
2024-10-21 10:10:50 +0200

    Remove header description.

--- app.py
+++ app.py
@@ -12,11 +12,9 @@ logger.configure(handlers=[{"sink": sys.stdout, "level": logging_level}])
 logger.info("Starting Gradio Demo")
 
 title = "A PlanQK Demo using Gradio!"
-description = '<div align="center"> <h1>A descriptive description!</h1> </div>'
 
 
 with gr.Blocks(title=title, theme=gr.themes.Soft()) as demo:
-    gr.Markdown(description)
     result_json_box_train = training_ui()
     prediction_ui(result_json_box_train)
 
